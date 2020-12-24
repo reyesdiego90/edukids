ac58405410c1c1b218873806558a85803eb99d20
Carlos Ortega <cortegah@miumg.edu.gt>
2020-12-24 13:20:13 -0600

    Se agrego datatable

--- admin.php
+++ admin.php
@@ -9,13 +9,17 @@
         <!-- ===== BOX ICONS ===== -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet'>
-        <link href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css' rel='stylesheet'>
-        <link href='https://cdn.datatables.net/1.10.22/css/jquery.dataTables.min.css' rel='stylesheet'>
         <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css">
+        <!-- ===== DataTable  ===== -->
+        
         <!-- ===== CSS ===== -->
         <link rel="stylesheet" href="css/admin.css">
-        <link rel="stylesheet" href="css/registro.css">
         
+        <link rel="stylesheet" href="css/registro.css">
+        <!-- ===== CSS DATATABLE ===== -->
+        <link rel="stylesheet" href="css/datatable.css">
         
 
         <title>Sidebar menu responsive</title>
@@ -88,11 +92,16 @@
        
         <!--===== MAIN JS =====-->
         
-        <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
+
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+        <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
         <script src="js/main.js"></script>
         <script src="js/dynamicPage.js"></script>
+        <script src="js/datatable.js"></script>
        
     </body>
 </html>
