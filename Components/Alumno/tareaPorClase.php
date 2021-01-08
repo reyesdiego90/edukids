828641ee76775563c70a9693fb673feb4af8d52b
Diego reyes <jreyesz778@gmail.com>
2021-01-07 21:00:41 -0600

    Quita la tarea si llega a su hora de vencimiento

--- Components/Alumno/tareaPorClase.php
+++ Components/Alumno/tareaPorClase.php
@@ -1,6 +1,28 @@
 <?php
   $base = mysqli_connect("127.0.0.1", "root", "toor", "Edukids", "3306");
   mysqli_set_charset($base, 'utf8'); 
+  $result = mysqli_query($base,'SELECT * FROM anuncio');
+  date_default_timezone_set('America/Guatemala');
+  setlocale(LC_ALL,"es_GT");
+  $dia = date("Y-m-d");
+  $hora = date("H:i:s");
+  echo $dia.' '.$hora;
+  while($res = mysqli_fetch_assoc($result)){
+    if($res["fechaEntrega"] <= $dia and $hora > $res["horaEntrega"] ){
+      echo ' '.$res['horaEntrega'];
+      $sqlEstado = "UPDATE anuncio 
+      SET estado_anuncio_id_estado = 2
+      WHERE id_anuncio = ".$res['id_anuncio'];
+      mysqli_query($base, $sqlEstado);
+    }else{
+      echo "jejej"; 
+      $sqlEstado = "UPDATE anuncio 
+      SET estado_anuncio_id_estado = 1
+      WHERE id_anuncio = ".$res["id_anuncio"];
+      mysqli_query($base, $sqlEstado);
+    }
+  }
+
   $result = mysqli_query($base, "SELECT * FROM anuncio 
   INNER JOIN curso ON curso_id_curso = id_curso
   INNER JOIN maestro ON maestro_id_maestro = id_maestro
